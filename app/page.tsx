026888642663be13c4ba466339e1364dce6b8d96
Anup Shrestha <anupshrestha40@gmail.com>
2026-01-28 13:49:24 +0545

    success

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
+
 import PalCutGame from "./palcut/page";
 
 export default function Home() {
- 
-
-
   return (
-   <div style={{ fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif` }}>
-   <PalCutGame/>
-   </div >
+    <div style={{ fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif` }}>
+      <PalCutGame />
+    </div>
   );
 }
