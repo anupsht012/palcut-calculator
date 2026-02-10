6a4201f4530cb56f84e7152931e0489db3e8840f
Anup Shrestha <anupshrestha40@gmail.com>
2026-02-10 09:39:53 +0545

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import PalCutGame from "./palcut/page";
 
 export default function Home() {
   return (
-    <div style={{ fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif` }}>
+    <div style={{ fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif` }}>1
+      <SpeedInsights />
       <PalCutGame />
     </div>
   );
